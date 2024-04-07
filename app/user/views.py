320d3b86ef2b3e54eaaa7f072d386b06270c4e46
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-04-08 05:50:13 +1000

    Push for checking code diff

---
+++ app/user/views.py
@@ -0,0 +1,9 @@
+"""Views for the user API"""
+
+from user.serializers import UserSerializer
+from rest_framework import generics
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
